6cf21f3167e3fab272e29c28ba49f7963844f23e
Mathias Norregaard <mathias.norregaard@ingka.ikea.com>
2024-09-21 11:33:59 +0200

    Decent rendering?

--- src/main/java/net/mcreator/ars_technica/common/items/equipment/SpyMonocleCurioRenderer.java
+++ src/main/java/net/mcreator/ars_technica/common/items/equipment/SpyMonocleCurioRenderer.java
@@ -2,6 +2,7 @@ package net.mcreator.ars_technica.common.items.equipment;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
 import com.simibubi.create.Create;
 import net.mcreator.ars_technica.ArsTechnicaMod;
@@ -16,6 +17,7 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemDisplayContext;
@@ -30,7 +32,8 @@ public class SpyMonocleCurioRenderer implements ICurioRenderer {
     public static final ModelLayerLocation SPY_MONOCLE_LAYER = new ModelLayerLocation(new ResourceLocation(ArsTechnicaMod.MODID, "spy_monocle"), "monocle");
     private final HumanoidModel<LivingEntity> model;
 
-    public SpyMonocleCurioRenderer(ModelPart part) {
+    public SpyMonocleCurioRenderer(ModelPart part)
+    {
         this.model = new HumanoidModel<>(part);
     }
 
@@ -38,43 +41,21 @@ public class SpyMonocleCurioRenderer implements ICurioRenderer {
     public <T extends LivingEntity, M extends EntityModel<T>> void render(ItemStack stack, SlotContext slotContext, PoseStack matrixStack, RenderLayerParent<T, M> renderLayerParent, MultiBufferSource renderTypeBuffer, int light, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
         model.setupAnim(slotContext.entity(), limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
         model.prepareMobModel(slotContext.entity(), limbSwing, limbSwingAmount, partialTicks);
-        ICurioRenderer.followHeadRotations(slotContext.entity(), model.head);
-
-        // Translate and rotate with our head
         matrixStack.pushPose();
-        matrixStack.translate(model.head.x, model.head.y, model.head.z);
-        matrixStack.translate(0.125, -0.125, -0.75);
-        matrixStack.mulPose(Axis.YP.rotation(model.head.yRot));
-        matrixStack.mulPose(Axis.XP.rotation(model.head.xRot));
-
-        // Translate and scale to our head
-        matrixStack.translate(0.125, -0.125, -0.75);
-        matrixStack.mulPose(Axis.ZP.rotationDegrees(180.0f));
-
-        // Render
-        model.head.render(matrixStack, renderTypeBuffer.getBuffer(RenderType.entityCutoutNoCull(new ResourceLocation(Create.ID, "textures/block/brass_casing.png"))), light, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
-
+        model.head.render(matrixStack, renderTypeBuffer.getBuffer(RenderType.entityCutoutNoCull(new ResourceLocation(ArsTechnicaMod.MODID, "textures/entity/spy_monocle.png"))), light, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
         matrixStack.popPose();
     }
 
     public static LayerDefinition createBodyLayer() {
-        return LayerDefinition.create(createSpyMonocleModel(), 1, 1);
-    }
-
-    public static MeshDefinition createSpyMonocleModel() {
         MeshDefinition mesh = HumanoidModel.createMesh(CubeDeformation.NONE, 0);
-        CubeListBuilder builder = CubeListBuilder.create();
+        PartDefinition partdefinition = mesh.getRoot();
 
-        builder.addBox(0.0F, 0.0F, 0.0F, 2.0F, 2.0F, 0.5F, new CubeDeformation(0.0F));
+        PartDefinition head = partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(7, 0).addBox(-3.0F, -4.9F, -4.5F, 2.0F, 2.0F, 0.5F, new CubeDeformation(0.0F))
+                .texOffs(8, 14).addBox(-2.9F, -4.8F, -4.7F, 1.8F, 1.8F, 0.2F, new CubeDeformation(0.0F))
+                .texOffs(11, 14).addBox(-2.8F, -4.7F, -4.9F, 1.6F, 1.6F, 0.2F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
 
-        builder.addBox(0.1F, 0.1F, -0.2F, 1.8F, 1.8F, 0.2F, new CubeDeformation(0.0F));
+        head.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(7, 7).addBox(-0.2F, -0.2F, 0.3F, 1.0F, 1.0F, 0.2F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-2.0F, -4.3F, -5.4F, 0.0F, 0.0F, 0.7854F));
 
-        builder.addBox(0.2F, 0.2F, -0.4F, 1.6F, 1.6F, 0.2F, new CubeDeformation(0.0F));
-
-        builder.addBox(0.2F, 0.6F, -0.6F, 1.0F, 1.0F, 0.2F, new CubeDeformation(0.0F));
-
-        mesh.getRoot().addOrReplaceChild("head", builder, PartPose.ZERO);
-
-        return mesh;
+        return LayerDefinition.create(mesh, 16, 16);
     }
 }
