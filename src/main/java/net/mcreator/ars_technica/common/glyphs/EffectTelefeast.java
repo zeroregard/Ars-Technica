c8a4b6e64351667606d8671a3f4babe662d9ceff
Mathias <mathias_sn@hotmail.com>
2024-12-11 10:39:05 +0100

    feat: added ability to use Sensitive for Telefeast to target 'usable' items like XP gems, ender pearls etc

--- src/main/java/net/mcreator/ars_technica/common/glyphs/EffectTelefeast.java
+++ src/main/java/net/mcreator/ars_technica/common/glyphs/EffectTelefeast.java
@@ -2,18 +2,22 @@ package net.mcreator.ars_technica.common.glyphs;
 
 import alexthw.starbunclemania.starbuncle.fluid.StarbyFluidBehavior;
 import com.hollingsworth.arsnouveau.api.spell.*;
+import com.hollingsworth.arsnouveau.common.spell.augment.AugmentSensitive;
 import net.mcreator.ars_technica.ArsTechnicaMod;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.animal.Cow;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.PotionItem;
@@ -52,6 +56,7 @@ public class EffectTelefeast extends AbstractEffect {
         var pos = rayTraceResult.getBlockPos();
         BlockEntity be = world.getBlockEntity(pos);
         LivingEntity consumer = shooter;
+        boolean canUse = spellStats.isSensitive(); // Will try to 'use' an item even if it's not a drink/food
 
         if (be != null && be.getCapability(ForgeCapabilities.FLUID_HANDLER).isPresent()) {
             IFluidHandler fluidHandler = StarbyFluidBehavior.getHandlerFromCap(pos, world, 0);
@@ -61,7 +66,7 @@ public class EffectTelefeast extends AbstractEffect {
         } else if (be != null && be.getCapability(ForgeCapabilities.ITEM_HANDLER).isPresent()) {
             IItemHandler itemHandler = be.getCapability(ForgeCapabilities.ITEM_HANDLER).orElse(null);
             if (itemHandler != null) {
-                handleItem(shooter, consumer, itemHandler, world);
+                handleItem(shooter, consumer, itemHandler, world, canUse);
             }
         }
     }
@@ -72,6 +77,59 @@ public class EffectTelefeast extends AbstractEffect {
             drinkSound(world, consumer);
             itemResult.finishUsingItem(world, consumer);
         }
+    }
+
+    private void handleItem(LivingEntity caster, LivingEntity consumer, IItemHandler itemHandler, Level world, boolean canUse) {
+        for (int i = 0; i < itemHandler.getSlots(); i++) {
+            ItemStack itemStack = itemHandler.getStackInSlot(i);
+            if (!itemStack.isEmpty()) {
+                if(tryUseConsumableItem(consumer, itemStack, world, canUse)) {
+                    break;
+                }
+
+                if (tryUseEdibleItem(consumer, itemStack, world)) {
+                    break;
+                }
+            }
+        }
+    }
+
+    private boolean tryUseConsumableItem(LivingEntity consumer, ItemStack itemStack, Level world, boolean canUse) {
+        if(!canUse) {
+            return false;
+        }
+
+        // While LivingEntities have interaction hands, only 'Player' can be passed for the 'use' function
+        if(consumer instanceof Player player) {
+            InteractionHand temporaryHand = InteractionHand.MAIN_HAND;
+            ItemStack originalItem = player.getItemInHand(temporaryHand);
+
+            try {
+                player.setItemInHand(temporaryHand, itemStack);
+                var result = itemStack.use(world, player, temporaryHand).getResult();
+                if (result != InteractionResult.FAIL) {
+                    return true;
+                }
+            } finally {
+                player.setItemInHand(temporaryHand, originalItem);
+            }
+            return false;
+        }
+        return false;
+    }
+
+    private boolean tryUseEdibleItem(LivingEntity consumer, ItemStack itemStack, Level world) {
+        if (itemStack.isEdible()) {
+            consumer.eat(world, itemStack);
+            world.playSound(null, consumer.getX(), consumer.getY(), consumer.getZ(), SoundEvents.GENERIC_EAT, SoundSource.PLAYERS, 1.0F, 1.0F);
+            return true;
+        } else if ((itemStack.getItem() instanceof PotionItem || itemStack.getUseAnimation() == UseAnim.DRINK)) {
+            itemStack.finishUsingItem(world, consumer);
+            world.playSound(null, consumer.getX(), consumer.getY(), consumer.getZ(),
+                    SoundEvents.GENERIC_DRINK, SoundSource.PLAYERS, 1.0F, 1.0F);
+            return true;
+        }
+        return false;
     }
 
     private void handleFluid(LivingEntity caster, LivingEntity consumer, FluidStack fluid, IFluidHandler handler, Level world) {
@@ -116,25 +174,6 @@ public class EffectTelefeast extends AbstractEffect {
                 SoundEvents.GENERIC_DRINK, SoundSource.PLAYERS, 1.0F, 1.0F);
     }
 
-    private void handleItem(LivingEntity caster, LivingEntity consumer, IItemHandler itemHandler, Level world) {
-        for (int i = 0; i < itemHandler.getSlots(); i++) {
-            ItemStack itemStack = itemHandler.getStackInSlot(i);
-            if (!itemStack.isEmpty()) {
-                if (itemStack.isEdible()) {
-                    consumer.eat(world, itemStack);
-                    world.playSound(null, consumer.getX(), consumer.getY(), consumer.getZ(), SoundEvents.GENERIC_EAT, SoundSource.PLAYERS, 1.0F, 1.0F);
-                    break;
-                } else if ((itemStack.getItem() instanceof PotionItem || itemStack.getUseAnimation() == UseAnim.DRINK)) {
-                    itemStack.finishUsingItem(world, consumer);
-                    world.playSound(null, consumer.getX(), consumer.getY(), consumer.getZ(),
-                            SoundEvents.GENERIC_DRINK, SoundSource.PLAYERS, 1.0F, 1.0F);
-                    break;
-                }
-            }
-        }
-    }
-
-
     @Override
     protected int getDefaultManaCost() {
         return 10;
@@ -142,7 +181,7 @@ public class EffectTelefeast extends AbstractEffect {
 
     @Override
     protected @NotNull Set<AbstractAugment> getCompatibleAugments() {
-        return Set.of();
+        return Set.of(AugmentSensitive.INSTANCE);
     }
 
     @Nonnull
@@ -153,6 +192,6 @@ public class EffectTelefeast extends AbstractEffect {
 
     @Override
     public String getBookDescription() {
-        return "Consumes the first edible/potion, or some amount of liquid, found in the container/tank this was cast on";
+        return "Consumes the first edible/potion, or some amount of liquid, found in the container/tank this was cast on. Augment with Sensitive for also targeting 'usable' items";
     }
 }
