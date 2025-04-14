8bcd07a30ea6f633ab148bc0136a7ab48c00dcfa
Mathias <mathias_sn@hotmail.com>
2025-04-14 19:16:21 +0200

    chore: bump required ars version to minimum 5.8

--- src/main/java/com/zeroregard/ars_technica/datagen/ArsProviders.java
+++ src/main/java/com/zeroregard/ars_technica/datagen/ArsProviders.java
@@ -1,29 +1,21 @@
 package com.zeroregard.ars_technica.datagen;
 
-import com.hollingsworth.arsnouveau.api.familiar.AbstractFamiliarHolder;
-import com.hollingsworth.arsnouveau.api.ritual.AbstractRitual;
-import com.hollingsworth.arsnouveau.api.spell.AbstractCastMethod;
-import com.hollingsworth.arsnouveau.api.spell.AbstractEffect;
-import com.hollingsworth.arsnouveau.api.spell.AbstractSpellPart;
+
 import com.hollingsworth.arsnouveau.common.crafting.recipes.EnchantingApparatusRecipe;
 import com.hollingsworth.arsnouveau.common.crafting.recipes.ImbuementRecipe;
 import com.hollingsworth.arsnouveau.common.datagen.ApparatusRecipeBuilder;
 import com.hollingsworth.arsnouveau.common.datagen.ApparatusRecipeProvider;
 import com.hollingsworth.arsnouveau.common.datagen.ImbuementRecipeProvider;
-import com.hollingsworth.arsnouveau.common.datagen.patchouli.*;
 import com.zeroregard.ars_technica.ArsTechnica;
 import com.zeroregard.ars_technica.item.PressurePerk;
 import com.zeroregard.ars_technica.recipe.TechnomancerArmorRecipe;
-import com.zeroregard.ars_technica.registry.GlyphRegistry;
 import com.zeroregard.ars_technica.registry.ItemRegistry;
 import net.minecraft.data.CachedOutput;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.DataProvider;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.level.ItemLike;
 import net.neoforged.neoforge.common.conditions.IConditionBuilder;
 import org.jetbrains.annotations.NotNull;
 
@@ -34,7 +26,6 @@ import java.util.concurrent.CompletableFuture;
 
 import static com.hollingsworth.arsnouveau.setup.registry.ItemsRegistry.*;
 import static com.hollingsworth.arsnouveau.setup.registry.ItemsRegistry.AIR_ESSENCE;
-import static com.hollingsworth.arsnouveau.setup.registry.RegistryHelper.getRegistryName;
 import static com.simibubi.create.AllItems.*;
 import static com.zeroregard.ars_technica.datagen.Setup.provider;
 
@@ -220,87 +211,6 @@ public class ArsProviders {
         @Override
         public @NotNull String getName() {
             return "Example Imbuement";
-        }
-
-    }
-
-    public static class PatchouliProvider extends com.hollingsworth.arsnouveau.common.datagen.PatchouliProvider {
-
-        public PatchouliProvider(DataGenerator generatorIn) {
-            super(generatorIn);
-        }
-
-        @Override
-        public void collectJsons(CachedOutput cache) {
-
-            for (AbstractSpellPart spell : GlyphRegistry.registeredSpells) {
-                addGlyphPage(spell);
-            }
-
-            for (PatchouliPage patchouliPage : pages) {
-                DataProvider.saveStable(cache, patchouliPage.build(), patchouliPage.path());
-            }
-
-        }
-
-        @Override
-        public PatchouliPage addBasicItem(ItemLike item, ResourceLocation category, IPatchouliPage recipePage) {
-            PatchouliBuilder builder = new PatchouliBuilder(category, item.asItem().getDescriptionId())
-                    .withIcon(item.asItem())
-                    .withPage(new TextPage(root + ".page." + getRegistryName(item.asItem()).getPath()))
-                    .withPage(recipePage);
-            var page = new PatchouliPage(builder, getPath(category, getRegistryName(item.asItem()).getPath()));
-            this.pages.add(page);
-            return page;
-        }
-
-        public void addFamiliarPage(AbstractFamiliarHolder familiarHolder) {
-            PatchouliBuilder builder = new PatchouliBuilder(FAMILIARS, "entity." + root + "." + familiarHolder.getRegistryName().getPath())
-                    .withIcon(root + ":" + familiarHolder.getRegistryName().getPath())
-                    .withTextPage(root + ".familiar_desc." + familiarHolder.getRegistryName().getPath())
-                    .withPage(new EntityPage(familiarHolder.getRegistryName().toString()));
-            this.pages.add(new PatchouliPage(builder, getPath(FAMILIARS, familiarHolder.getRegistryName().getPath())));
-        }
-
-        public void addRitualPage(AbstractRitual ritual) {
-            PatchouliBuilder builder = new PatchouliBuilder(RITUALS, "item." + root + '.' + ritual.getRegistryName().getPath())
-                    .withIcon(ritual.getRegistryName().toString())
-                    .withTextPage(ritual.getDescriptionKey())
-                    .withPage(new CraftingPage(root + ":tablet_" + ritual.getRegistryName().getPath()));
-
-            this.pages.add(new PatchouliPage(builder, getPath(RITUALS, ritual.getRegistryName().getPath())));
-        }
-
-        public void addGlyphPage(AbstractSpellPart spellPart) {
-            ResourceLocation category = switch (spellPart.defaultTier().value) {
-                case 1 -> GLYPHS_1;
-                case 2 -> GLYPHS_2;
-                default -> GLYPHS_3;
-            };
-            PatchouliBuilder builder = new PatchouliBuilder(category, spellPart.getName())
-                    .withName(root + ".glyph_name." + spellPart.getRegistryName().getPath())
-                    .withIcon(spellPart.getRegistryName().toString())
-                    .withSortNum(spellPart instanceof AbstractCastMethod ? 1 : spellPart instanceof AbstractEffect ? 2 : 3)
-                    .withPage(new TextPage(root + ".glyph_desc." + spellPart.getRegistryName().getPath()))
-                    .withPage(new GlyphScribePage(spellPart));
-            this.pages.add(new PatchouliPage(builder, getPath(category, spellPart.getRegistryName().getPath())));
-        }
-
-        /**
-         * Gets a name for this provider, to use in logging.
-         */
-        @Override
-        public @NotNull String getName() {
-            return "Example Patchouli Datagen";
-        }
-
-        @Override
-        public Path getPath(ResourceLocation category, String fileName) {
-            return this.generator.getPackOutput().getOutputFolder().resolve("data/" + root + "/patchouli_books/example/en_us/entries/" + category.getPath() + "/" + fileName + ".json");
-        }
-
-        ImbuementPage ImbuementPage(ItemLike item) {
-            return new ImbuementPage(root + ":imbuement_" + getRegistryName(item.asItem()).getPath());
         }
 
     }
