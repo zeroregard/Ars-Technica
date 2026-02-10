2d7d567463f07514429322c86c07428b5d7265e9
zerobuncle <mathias_sn@hotmail.com>
2026-02-10 09:02:11 +0100

    fix: relay not turning into precise relay when wrenched; other relays than regular relay being able to be turned into precise relay

--- src/main/java/com/zeroregard/ars_technica/mixin/RelayBlockMixin.java
+++ src/main/java/com/zeroregard/ars_technica/mixin/RelayBlockMixin.java
@@ -2,6 +2,7 @@ package com.zeroregard.ars_technica.mixin;
 
 import com.hollingsworth.arsnouveau.client.particle.ParticleColor;
 import com.hollingsworth.arsnouveau.common.block.Relay;
+import com.simibubi.create.content.equipment.wrench.IWrenchable;
 import com.zeroregard.ars_technica.Config;
 import com.zeroregard.ars_technica.block.PreciseRelay;
 import com.zeroregard.ars_technica.block.PreciseRelayTile;
@@ -12,19 +13,44 @@ import com.zeroregard.ars_technica.registry.ParticleRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 
 import static com.simibubi.create.foundation.utility.BlockHelper.copyProperties;
 
 @Mixin(Relay.class)
-public abstract class RelayBlockMixin implements IArsTechnicaWrenchAdjustable {
+public abstract class RelayBlockMixin implements IArsTechnicaWrenchAdjustable, IWrenchable {
+
+    @Override
+    public InteractionResult onWrenched(BlockState state, UseOnContext context) {
+        Level level = context.getLevel();
+        BlockPos pos = context.getClickedPos();
+        Player player = context.getPlayer();
+        if (player == null)
+            return InteractionResult.PASS;
+        // Only base Source Relay and PreciseRelay react; other subclasses (e.g. Creative Relay) do nothing
+        Class<?> blockClass = ((Object) this).getClass();
+        if (blockClass != Relay.class && blockClass != PreciseRelay.class)
+            return InteractionResult.PASS;
+        ((IArsTechnicaWrenchAdjustable) this).handleWrenching(level, pos, player);
+        return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public InteractionResult onSneakWrenched(BlockState state, UseOnContext context) {
+        return InteractionResult.PASS;
+    }
+
     public void handleWrenching(Level world, BlockPos pos, Player player) {
+        // Only the base Source Relay may be converted; subclasses (e.g. Creative Relay) must not
+        if (((Object) this).getClass() != Relay.class)
+            return;
         if (!world.isClientSide) {
             HolderLookup.Provider registries = world.registryAccess();
             BlockState oldState = world.getBlockState(pos);
