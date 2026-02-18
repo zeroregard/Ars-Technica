bf34b58ff8908837e5894dee773d3afbd98aa3e3
zerobuncle <mathias_sn@hotmail.com>
2026-02-18 08:16:50 +0100

    fix: Sauce library dependency not being jarjar'd, potentially causing crashing

--- src/main/java/com/zeroregard/ars_technica/ArsNouveauRegistry.java
+++ src/main/java/com/zeroregard/ars_technica/ArsNouveauRegistry.java
@@ -1,21 +1,13 @@
 package com.zeroregard.ars_technica;
 
+import com.alexthw.sauce.registry.ModRegistry;
 import com.hollingsworth.arsnouveau.api.ArsNouveauAPI;
 import com.hollingsworth.arsnouveau.api.perk.PerkSlot;
 import com.hollingsworth.arsnouveau.api.registry.PerkRegistry;
-import com.hollingsworth.arsnouveau.common.items.data.ArmorPerkHolder;
-import com.zeroregard.ars_technica.glyphs.EffectCarve;
-import com.zeroregard.ars_technica.glyphs.EffectPack;
-import com.zeroregard.ars_technica.glyphs.EffectPolish;
 import com.zeroregard.ars_technica.item.PressurePerk;
 import com.zeroregard.ars_technica.registry.ItemRegistry;
-import com.hollingsworth.arsnouveau.api.registry.GlyphRegistry;
-import com.hollingsworth.arsnouveau.api.registry.SpellSoundRegistry;
-import com.hollingsworth.arsnouveau.api.spell.AbstractSpellPart;
-import com.alexthw.sauce.registry.ModRegistry;
 import net.minecraft.world.level.ItemLike;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
